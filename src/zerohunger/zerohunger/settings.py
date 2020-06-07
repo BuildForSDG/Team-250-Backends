8aab7a7f76a33fd15b5d3ea614486f50787fba4b
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-06-07 04:46:50 +0100

    add pagination

--- src/zerohunger/zerohunger/settings.py
+++ src/zerohunger/zerohunger/settings.py
@@ -94,7 +94,8 @@ AUTH_USER_MODEL = 'accounts.User'
 DATABASES = {}
 
 DATABASES['default'] = dj_database_url.config(
-    default=os.getenv('DATABASE_URL'))
+    default=os.getenv('DATABASE_URL'), ssl_require=True)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
